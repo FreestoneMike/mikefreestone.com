acebc25be2d01f668721c6343380ec8ae1d83914
Mike Freestone <mikefreestone@gmail.com>
2021-04-21 16:41:57 -0400

    Style Contact page and add email button and links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+
+// ASSET IMPORTS
+import ProfilePhoto from "../assets/images/profile-photo.jpg"
+import GitHubLogo from "../assets/icons/GitHub-Mark-64px.png"
+import LinkedInLogo from "../assets/icons/linked-in-logo.png"
+
+// STYLES IMPORT
+import "./Contact.scss";
 
 function Contact() {
   return (
-    <h1>Contact Page</h1>
+    <section className="contact">
+      <div className="contact__container">
+        <div className="contact__img-container">
+          <img src={ProfilePhoto} alt="mike freestone" className="contact__img"/>
+        </div>
+        <div className="contact__info-container">
+          <h4 className="contact__greeting--title">Hey, thanks for visiting my site!<br/></h4>
+          <p className="contact__greeting">If you'd like to learn more about the projects I've worked on, or want to chat about everything sports, click this button to email me or reach out through any of the platforms below.</p>
+          
+          <form action="mailto:mikefreestone@gmail.com">
+            <input className="contact__btn" type="submit" value="Email" />
+          </form>
+          
+          <div className="contact_icon-container">
+            <a href="https://github.com/FreestoneMike" target="blank">
+              <img src={GitHubLogo} className="contact__icon" alt=""/>
+            </a>
+            <a href="https://www.linkedin.com/in/michaelfreestone/" target="blank">
+              <img src={LinkedInLogo} className="contact__icon" alt=""/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
